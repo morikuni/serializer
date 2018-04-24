536891c4263058e351c44b87a654df7abfaeb6b7
morikuni <taihei.with+git@gmail.com>
2018-04-24 14:09:16 +0900

    Add doc comment for option

--- option.go
+++ option.go
@@ -1,19 +1,23 @@
 package serializer
 
+// Option represents a option of the serializer.
 type Option func(s *Serializer)
 
+// WithMarshaler changes the marshaler.
 func WithMarshaler(m Marshaler) Option {
 	return func(s *Serializer) {
 		s.marshaler = m
 	}
 }
 
+// WithEncoder changes the encoder.
 func WithEncoder(e Encoder) Option {
 	return func(s *Serializer) {
 		s.encoder = e
 	}
 }
 
+// WithTypeNameResolver changes the resolver.
 func WithTypeNameResolver(r TypeNameResolver) Option {
 	return func(s *Serializer) {
 		s.resolver = r
