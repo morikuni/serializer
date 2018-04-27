50cb0d0a9427dd404bdc99af0790483be286d423
morikuni <taihei.with+git@gmail.com>
2018-04-27 15:35:47 +0900

    Rename ID to Name

--- encoder.go
+++ encoder.go
@@ -7,8 +7,8 @@ import (
 
 // Encoder encodes and decodes a Data.
 type Encoder interface {
-	Encode(w io.Writer, id string, v interface{}) error
-	Decode(r io.Reader) (id string, payload []byte, err error)
+	Encode(w io.Writer, name string, v interface{}) error
+	Decode(r io.Reader) (name string, payload []byte, err error)
 	Unmarshal(payload []byte, v interface{}) error
 }
 
@@ -20,19 +20,19 @@ func NewJSONEncoder() Encoder {
 
 type jsonEncoder struct{}
 
-func (jsonEncoder) Encode(w io.Writer, id string, v interface{}) error {
+func (jsonEncoder) Encode(w io.Writer, name string, v interface{}) error {
 	type encodingJSON struct {
-		ID      string      `json:"id"`
+		Name    string      `json:"name"`
 		Payload interface{} `json:"payload"`
 	}
 
-	d := encodingJSON{id, v}
+	d := encodingJSON{name, v}
 	return json.NewEncoder(w).Encode(d)
 }
 
 func (jsonEncoder) Decode(r io.Reader) (string, []byte, error) {
 	type decodingJSON struct {
-		ID      string          `json:"id"`
+		Name    string          `json:"name"`
 		Payload json.RawMessage `json:"payload"`
 	}
 
@@ -40,7 +40,7 @@ func (jsonEncoder) Decode(r io.Reader) (string, []byte, error) {
 	if err := json.NewDecoder(r).Decode(&d); err != nil {
 		return "", nil, err
 	}
-	return d.ID, d.Payload, nil
+	return d.Name, d.Payload, nil
 }
 
 func (jsonEncoder) Unmarshal(payload []byte, v interface{}) error {
